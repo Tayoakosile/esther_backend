20821debe09ba67d15587fb1389188137eac3291
Tayo Akosile <oluwatayoakosile@gmail.com>
2025-10-13 20:53:08 +0100

    add authentication and user management modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule],
+  controllers: [AppController, UsersController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
+export class AuthService {
+  createAccount(body: any): string {
+    console.log('body :', body);
+
     const condition = false; // Replace with your actual condition
     if (condition) {
       throw new BadRequestException('Error Here');
