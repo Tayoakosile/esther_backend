745f7c52f07d404bb72dfd657974600add4a8946
Tayo Akosile <oluwatayoakosile@gmail.com>
2025-10-15 22:19:36 +0100

    feat: updated app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // local frontend
+      'http://localhost:3002', // local frontend
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // only if you use cookies or Authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(' Server listening on port', process.env.PORT ?? 3000);
 }
